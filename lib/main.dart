c149a053316b6b2aab417d87a7f403ab26a3a743
henrique945 <henrique.rodrigues7@hotmail.com>
2021-04-29 22:58:23 -0300

    update(main): add StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Hello World',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.green, fontSize: 50),
-      ),
-    ),
-    color: Colors.black,
+  runApp(AppWidget(
+    title: 'Hello World',
   ));
+}
+
+// Widget Imutavel
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.green, fontSize: 50),
+        ),
+      ),
+      color: Colors.black,
+    );
+  }
 }
